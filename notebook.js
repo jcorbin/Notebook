78ea468d48427cf7a10ef2a5a40019bcb1de09bc
Joshua T Corbin <jcorbin@wunjo.org>
2009-11-29 16:42:53 -0500

    Add Notebook.Layer

--- notebook.js
+++ notebook.js
@@ -1,6 +1,7 @@
 goog.provide('wunjo.Notebook');
 
 goog.require('goog.array');
+goog.require('goog.events.EventTarget');
 
 wunjo.Notebook.Page = function(width, height, options) {
   this.width_ = width;
@@ -101,6 +102,137 @@ wunjo.Notebook.Page.prototype.clear = function() {
 };
 
 
+wunjo.Notebook.Layer = function(name) {
+  goog.events.EventTarget.call(this);
+
+  this.name_ = name;
+};
+goog.inherits(wunjo.Notebook.Layer, goog.events.EventTarget);
+
+wunjo.Notebook.Layer.itemTypes = {};
+
+wunjo.Notebook.Layer.getItemType = function(item) {
+  for (type in this.itemTypes) {
+    if (item instanceof this.itemTypes[type]) {
+      return type;
+    }
+  }
+  return null;
+};
+
+wunjo.Notebook.Layer.unserialize = function(data) {
+  var layer = new wunjo.Notebook.Layer(data.name);
+  if (data.items) {
+    var items = [];
+    for (var i=0; i<data.items.length; i++) {
+      var itemtype = data.items[i].type;
+      if (! itemtype in this.itemTypes) {
+        throw Error('invalid item type '+itemtype);
+      }
+      items.push(
+        this.itemTypes[itemtype].unserialize(data.items[i])
+      );
+    }
+    if (items.length) {
+      layer.items_ = items;
+    }
+  }
+  return layer;
+};
+
+wunjo.Notebook.Layer.prototype.items_ = null;
+
+wunjo.Notebook.Layer.prototype.serialize = function() {
+  var data = {
+    name: this.name_
+  };
+  if (this.items_) {
+    data.items = [];
+    for (var i=0; i<this.items_.length; i++) {
+      var itemdata = this.items_[i].serialize();
+      itemdata.type = wunjo.Notebook.Layer.getItemType(this.items_[i]);
+      data.items.push(itemdata);
+    }
+  }
+  return data;
+};
+
+wunjo.Notebook.Layer.prototype.getName = function() {
+  return this.name_;
+};
+
+wunjo.Notebook.Layer.prototype.setName = function(name) {
+  this.name_ = name;
+  this.dispatchEvent({
+    type: 'namechanged',
+    value: name
+  });
+};
+
+wunjo.Notebook.Layer.prototype.draw = function(canvas) {
+  for (var i=0; i<this.items_.length; i++) {
+    this.items_[i].draw(canvas);
+  }
+};
+
+wunjo.Notebook.Layer.prototype.getItemCount = function() {
+  if (! this.items_) return 0;
+  return this.items_.length;
+};
+
+wunjo.Notebook.Layer.prototype.getItem = function(i) {
+  if (! this.items_) return null;
+  return this.items_[i];
+};
+
+wunjo.Notebook.Layer.prototype.getItems = function() {
+  if (! this.items_) return null;
+  return goog.array.clone(this.items_);
+};
+
+wunjo.Notebook.Layer.prototype.clear = function() {
+  while (this.items_.length) {
+    this.removeItem(this.items_[0]);
+  }
+  this.items_ = null;
+};
+
+wunjo.Notebook.Layer.prototype.addItem = function(item) {
+  if (! this.items_) {
+    this.items_ = [];
+  }
+  goog.array.insert(this.items_, item);
+  this.dispatchEvent({
+    type: 'itemadded',
+    item: item
+  });
+  return item;
+};
+
+wunjo.Notebook.Layer.prototype.addItemAt = function(item, i) {
+  if (! this.items_) {
+    this.items_ = [];
+  }
+  goog.array.insertAt(this.items_, item, i);
+  this.dispatchEvent({
+    type: 'itemadded',
+    item: item
+  });
+  return item;
+};
+
+wunjo.Notebook.Layer.prototype.removeItem = function(item) {
+  goog.array.remove(this.items_, item);
+  if (! this.items_.length) {
+    this.items_ = null;
+  }
+  this.dispatchEvent({
+    type: 'itemremoved',
+    item: item
+  });
+};
+
+
 wunjo.Notebook.Stroke = function(width, color, startX, startY) {
   this.color = color;
   this.width = width;
@@ -109,6 +241,8 @@ wunjo.Notebook.Stroke = function(width, color, startX, startY) {
     this.addPoint(startX, startY);
   }
 };
+
+wunjo.Notebook.Layer.itemTypes['stroke'] = wunjo.Notebook.Stroke;
 
 wunjo.Notebook.Stroke.unserialize = function(data) {
   var stroke = new wunjo.Notebook.Stroke(data.width, data.color);
