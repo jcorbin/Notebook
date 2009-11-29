dd95f784c54a0847962adc6e91bcda7525a2fcc1
Joshua T Corbin <jcorbin@wunjo.org>
2009-11-29 10:44:42 -0500

    Pass around just the canvas in drawing
    Instead of passing around the notebook object, just pass around the
    canvas instead.
    
    Necessitates storing the DPI on the canvas object, which turns out to be
    good enough for everyone that cared anyhow.

--- notebook.js
+++ notebook.js
@@ -6,12 +6,12 @@ wunjo.Notebook = function(canvas) {
   }
 
   this.canvas = canvas;
-  this.dpi = wunjo.Notebook.getDPI();
+  this.canvas.dpi = wunjo.Notebook.getDPI();
   this.currentStroke = null;
   this.currentPage = null;
 
   if (! this.restore()) {
-    this.currentPage = new wunjo.Notebook.Page(8.5*this.dpi, 11*this.dpi);
+    this.currentPage = new wunjo.Notebook.Page(8.5*this.canvas.dpi, 11*this.canvas.dpi);
   }
 
   this.canvas.width = this.currentPage.width;
@@ -144,7 +144,7 @@ wunjo.Notebook.prototype.redraw = function() {
   this.draw();
 };
 wunjo.Notebook.prototype.draw = function() {
-  this.currentPage.draw(this);
+  this.currentPage.draw(this.canvas);
 };
 wunjo.Notebook.prototype.clearPage = function() {
   if (! this.currentPage) return;
@@ -188,43 +188,43 @@ wunjo.Notebook.Page.prototype.serialize = function() {
   }
   return data;
 };
-wunjo.Notebook.Page.prototype.draw = function(nb) {
+wunjo.Notebook.Page.prototype.draw = function(canvas) {
   if (typeof(this.options.paper) == "function") {
-    this.options.paper(this, nb);
+    this.options.paper(this, canvas);
   } else {
-    this.drawPaper(nb);
+    this.drawPaper(canvas);
   }
 
   // Draw strokes
   for (var i=0; i<this.strokes.length; i++) {
-    this.strokes[i].draw(nb);
+    this.strokes[i].draw(canvas);
   }
 };
-wunjo.Notebook.Page.prototype.drawPaper = function(nb) {
+wunjo.Notebook.Page.prototype.drawPaper = function(canvas) {
   if (this.options.paper == "blank") return;
 
-  var ctx = nb.canvas.getContext('2d');
+  var ctx = canvas.getContext('2d');
 
   switch (this.options.paper) {
     case 'ruled':
-      var margin = 4/5*nb.dpi, marginRuleWidth = 3;
+      var margin = 4/5*canvas.dpi, marginRuleWidth = 3;
       ctx.fillStyle = 'rgba(192, 64, 64, 0.25)';
-      ctx.fillRect(margin - marginRuleWidth/2, 0, marginRuleWidth, nb.canvas.height);
+      ctx.fillRect(margin - marginRuleWidth/2, 0, marginRuleWidth, canvas.height);
     case 'lined':
-      var spacing = nb.dpi/5, width = 2, half=width/2, start = nb.dpi;
+      var spacing = canvas.dpi/5, width = 2, half=width/2, start = canvas.dpi;
       ctx.fillStyle = 'rgba(64, 64, 192, 0.25)';
-      for (var i=start; i<nb.canvas.height; i+=spacing) {
-        ctx.fillRect(0, i-half, nb.canvas.width, width);
+      for (var i=start; i<canvas.height; i+=spacing) {
+        ctx.fillRect(0, i-half, canvas.width, width);
       }
       break;
     case 'grid':
-      var spacing = nb.dpi/5, width = 2, half = width/2;
+      var spacing = canvas.dpi/5, width = 2, half = width/2;
       ctx.fillStyle = 'rgba(32, 96, 32, 0.25)';
-      for (var i=spacing; i<nb.canvas.width; i+=spacing) {
-        ctx.fillRect(i-half, 0, width, nb.canvas.height);
+      for (var i=spacing; i<canvas.width; i+=spacing) {
+        ctx.fillRect(i-half, 0, width, canvas.height);
       }
-      for (var i=spacing; i<nb.canvas.height; i+=spacing) {
-        ctx.fillRect(0, i-half, nb.canvas.width, width);
+      for (var i=spacing; i<canvas.height; i+=spacing) {
+        ctx.fillRect(0, i-half, canvas.width, width);
       }
       break;
   }
@@ -269,9 +269,9 @@ wunjo.Notebook.Stroke.prototype.serialize = function() {
 wunjo.Notebook.Stroke.prototype.addPoint = function(x, y) {
   this.points.push([x, y]);
 };
-wunjo.Notebook.Stroke.prototype.draw = function(nb) {
+wunjo.Notebook.Stroke.prototype.draw = function(canvas) {
   if (this.points.length <= 1) return;
-  ctx = nb.canvas.getContext('2d');
+  ctx = canvas.getContext('2d');
   ctx.strokeStyle = this.color;
   ctx.lineWidth = this.width;
   var half = this.width/2;
